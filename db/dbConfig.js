a14481be9595110920f31c99bb328a9f71df990a
MontageD <guandexis@gmail.com>
2020-03-25 23:35:19 +0800

    修改数据库参数

---
+++ db/dbConfig.js
@@ -0,0 +1,10 @@
+module.exports =
+  {
+    mysql: {
+      host: 'localhost',   // 数据库ip 地址
+      user: 'root',             // 数据库用户名
+      password: '***', // 数据库密码
+      database: 'maoping', // 数据库表
+      port: 3306           // 数据库端口号
+    }
+  }
